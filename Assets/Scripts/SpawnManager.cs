5cc385eeb931d06eaa32a097e67779faabc9e5ca
Mephilos <sth4628@hotmail.com>
2025-04-25 01:28:35 +0900

    Prototype4 End, Challenge4 Start

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,20 +2,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public int enemyCount;
+    public int waveNumber;
     private float spawnRange = 9.0f;
     public GameObject enemyPrefabs;
+    public GameObject powerupPrefabs;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        Instantiate(powerupPrefabs, GenerateSpawnPosition(), powerupPrefabs.transform.rotation);
+        EnemySpawnWave(waveNumber);
         
     }
-
+    void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if(enemyCount == 0)
+        {
+            ++waveNumber;
+            EnemySpawnWave(waveNumber);
+            Instantiate(powerupPrefabs, GenerateSpawnPosition(), powerupPrefabs.transform.rotation);
+        }
+    }
+    // Update is called once per frame
+    void EnemySpawnWave(int enemyNum)
+    {
+        for(int i = 0; i < enemyNum ; i++)
+        {
+            Instantiate(enemyPrefabs, GenerateSpawnPosition(), enemyPrefabs.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
@@ -23,4 +39,7 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
+
+
+    
 }
