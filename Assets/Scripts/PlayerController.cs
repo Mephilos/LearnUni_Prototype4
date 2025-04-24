6adbcef067d9167402909209e3abe50a0612df11
Mephilos <sth4628@hotmail.com>
2025-04-24 16:58:41 +0900

    add Powerup Indicator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveForce;
     public bool hasPowerup;
+    public GameObject powerIndicator;
+    private float powerStrength = 15.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +20,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float inputVertical = Input.GetAxis("Vertical");
+        powerIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
         playerRb.AddForce(focalPoint.transform.forward * inputVertical * moveForce);
     }
 
@@ -24,15 +28,28 @@ public class PlayerController : MonoBehaviour
     {
         if(other.CompareTag("Powerup"))
         {
+            
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerIndicator.gameObject.SetActive(true);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;
             Debug.Log("대상: " + collision.gameObject.name + "파워업 상태: " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerStrength, ForceMode.Impulse);
         }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        powerIndicator.gameObject.SetActive(false);
+        hasPowerup = false;
     }
 }
